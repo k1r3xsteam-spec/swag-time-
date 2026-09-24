e82b36194889975cdf399b5baaa0e16264077133
k1r3xsteam-spec <k1r3x.steam@gmail.com>
2026-09-24 23:55:14 +0300

    Create app/lib/api.dart

---
+++ app/lib/api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class Api {
+  static const base = "http://10.0.2.2:8000";
+
+  static Future<Map<String, dynamic>> username(String name) => _get("/username/$name");
+  static Future<Map<String, dynamic>> email(String addr) => _get("/email/$addr");
+  static Future<Map<String, dynamic>> domain(String dom) => _get("/domain/$dom");
+  static Future<Map<String, dynamic>> url(String url) =>
+      _get("/url?url=${Uri.encodeComponent(url)}");
+  static Future<Map<String, dynamic>> phone(String num) => _get("/phone/$num");
+
+  static Future<Map<String, dynamic>> exif(File f) async {
+    final req = http.MultipartRequest("POST", Uri.parse("$base/exif"))
+      ..files.add(await http.MultipartFile.fromPath("file", f.path));
+    final res = await req.send();
+    return jsonDecode(await res.stream.bytesToString());
+  }
+
+  static Future<Map<String, dynamic>> _get(String path) async {
+    final r = await http.get(Uri.parse("$base$path"));
+    return jsonDecode(r.body) as Map<String, dynamic>;
+  }
+}
